7e6e4ad66a7c51a49efd33e5952f1568a9e7c5ea
Isaac <imtz90b@gmail.com>
2015-03-01 02:19:35 -0600

    added feeds view, added cities to backend

--- city-notifications/app/channels/channels_service.py
+++ city-notifications/app/channels/channels_service.py
@@ -1,8 +1,10 @@
 import ferris3 as f3
 import protopigeon
 from protorpc import messages
+from google.appengine.ext import ndb
 from ferris3 import Model, Service, hvild, auto_service
 from app.models.channel import Channel
+from app.models.city import City
 
 ChannelMessage = f3.model_message(Channel)
 MultiMessage = protopigeon.list_message(ChannelMessage)
@@ -17,7 +19,7 @@ class ChannelsService(Service):
 
 
 	@f3.auto_method(returns= MultiMessage, http_method="GET", name="get_featured")
-	def by_channels(delf,request,):
+	def by_channels(self,request,):
 		
 		cha_msgs= Channel.query(
 			Channel.is_featured == True
@@ -27,3 +29,18 @@ class ChannelsService(Service):
 			return f3.messages.serialize_list(MultiMessage, cha_msgs)
 		else:
 			 raise f3.NotFoundException()
+
+
+	@f3.auto_method(returns= MultiMessage, http_method="GET", name="get_by_city")
+	def by_city(self,request,  city=(str,), country=(str,)):
+		parsed_city_key= (city+"-"+country).lower()
+		city_key= ndb.Key(City,parsed_city_key)
+		if(city_key):
+			cha_msgs= Channel.query(
+				Channel.city == city_key
+			)
+			
+			if cha_msgs is not None:
+				return f3.messages.serialize_list(MultiMessage, cha_msgs)
+			else:
+				 raise f3.NotFoundException()
--- city-notifications/app/models/channel.py
+++ city-notifications/app/models/channel.py
@@ -1,10 +1,11 @@
 from google.appengine.ext import ndb
 from ferris3 import Model
-
+from app.models.city import City
 
 class Channel(Model):
     name = ndb.StringProperty(required=True)
     short_id= ndb.StringProperty(required=True)
+    city = ndb.KeyProperty(kind='City')
     logo_url = ndb.StringProperty()
     admin_account = ndb.StringProperty()
     is_featured = ndb.BooleanProperty(default=False, required=True)
